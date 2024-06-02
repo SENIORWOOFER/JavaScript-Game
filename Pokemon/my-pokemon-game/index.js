415f7d8109ae73ae9fcf5e2df034c0dc9f86c726
SENIORWOOFER <edanloh28@gmail.com>
2024-06-02 16:57:53 +0800

    Player Created

--- Pokemon/my-pokemon-game/index.js
+++ Pokemon/my-pokemon-game/index.js
@@ -9,6 +9,21 @@ context.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = './images/Pallet Town.png'
+
+const playerImage = new Image()
+playerImage.src = './images/playerDown.png'
+
 image.onload = () => {
-  context.drawImage(image, -1300, -800)
+  context.drawImage(image, -1340, -800)
+  context.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width/4,
+    playerImage.height, 
+    canvas.width/2 - playerImage.width/8, 
+    canvas.height/2 - playerImage.height/2,
+    playerImage.width/4,
+    playerImage.height)
 }
+
