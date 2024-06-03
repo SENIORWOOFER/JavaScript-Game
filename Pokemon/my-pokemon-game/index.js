15eca811b231e7965b9bb5371fcd30149f2683e2
SENIORWOOFER <edanloh28@gmail.com>
2024-06-03 21:23:23 +0800

    Setting up for battle activation

--- Pokemon/my-pokemon-game/index.js
+++ Pokemon/my-pokemon-game/index.js
@@ -9,7 +9,10 @@ for(let i = 0; i < collisions.length; i+=70){
   collisionsMap.push(collisions.slice(i, 70 + i))
 }
 
-
+const battleZonesMap = []
+for(let i = 0; i < battleZonesData.length; i+=70){
+  battleZonesMap.push(battleZonesData.slice(i, 70 + i))
+}
 
 const boundaries = []
 const offset = {
@@ -20,6 +23,18 @@ collisionsMap.forEach((row, i) => {
   row.forEach((symbol, j) => {
     if(symbol === 1025)
       boundaries.push(new Boundary({position:{
+        x:j*Boundary.width + offset.x,
+        y:i*Boundary.height + offset.y
+    }}))
+  })
+})
+
+const battleZones = []
+
+battleZonesMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if(symbol === 1025)
+      battleZones.push(new Boundary({position:{
         x:j*Boundary.width + offset.x,
         y:i*Boundary.height + offset.y
     }}))
@@ -96,7 +111,7 @@ const keys = {
 }
 
 
-const movables = [background, ...boundaries, foreground]
+const movables = [background, ...boundaries, foreground, ...battleZones]
 
 function rectangularCollision({rectangle1, rectangle2}) {
   return (
@@ -113,8 +128,27 @@ function animateMovement(){
   boundaries.forEach(boundary => {
     boundary.draw()
   })
+  battleZones.forEach((battleZone) => {
+    battleZone.draw()
+  })
   player.draw()
   foreground.draw()
+
+  if(keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+    for (let i = 0; i < battleZones.length; i++){
+      const battleZone = battleZones[i]
+      const overlappingArea = (Math.min(player.position.x + player.width, battleZone.position.x + battleZone.width) - Math.max(player.position.x, battleZone.position.x)) * (Math.min(player.position.y + player.height, battleZone.position.y + battleZone.height) - Math.max(player.position.y, battleZone.position.y))
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: battleZone
+        }) && 
+        overlappingArea > (player.width * player.height) / 2 && Math.random() > 0.01
+      ){
+        break
+      }
+    }
+  }
 
   let moving = true
   player.moving = false
@@ -136,6 +170,7 @@ function animateMovement(){
         break
       }
     }
+
     if (moving)
       movables.forEach((movables) => {
         movables.position.y += 3
